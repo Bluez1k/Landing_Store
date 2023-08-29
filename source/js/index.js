44a4c92db563046db566f2f61339ed23b1f9c1d8
Blister-exist <teyvaz0kenaz@gmail.com>
2023-08-29 17:48:05 +0300

    added arrows

--- source/js/index.js
+++ source/js/index.js
@@ -1,59 +1,82 @@
-const frames = {
-    step: 100,
-    units: [
+const frames = {                                            //глобальный объект
+    step: 100,                                              //шаг сдвига (100% т.е. ширина блока)
+
+leftArrow: document.getElementById('leftArrow'),            //объект левой стрелки
+    rightArrow: document.getElementById('rightArrow'),      //объект правой стрелки
+
+    units: [                                                //объект содержащий объекты слайдов
         {
-        item: document.getElementById('b-1'),
-        startPos: 0,
-        endPos: -300,
-        curPos: 0,
-        // bullit: document.getElementById('bullit_1')
+        item: document.getElementById('b-1'),               //сам html объект
+        startPos: 0,                                        //старновая позиция
+        endPos: -300,                                       //конечная позиция
+        curPos: 0,                                          //текукщая позиция (!!!здесь всегда равна стартовой!!!)
         },
         {
-        item: document.getElementById('b-2'),
+        item: document.getElementById('b-2'),               //остальные по аналогии
         startPos: 100,
-        endPos: -200,
-        curPos: 100,
-        // bullit: document.getElementById('bullit_2')
-        },
+        endPos: -200,                                       //при добавлении нового слайда
+        curPos: 100,                                        //добавить новый объект по этоим шаблонам
+        },                                                  //не забыть указать старт текущий и конечный 
         {
         item: document.getElementById('b-3'),
         startPos: 200,
         endPos: -100,
         curPos: 200,
-        // bullit: document.getElementById('bullit_3'),
         },
         {
         item: document.getElementById('b-4'),
         startPos: 300,
         endPos: 0,
         curPos: 300,
-        // bullit: document.getElementById('bullit_1')
         },
     ],
 
 
-    changeBackgroundAuto: function(){
-        for (item in this.units){
-            let obj = this.units[item];
-            if (obj.curPos <= obj.endPos){
-                obj.curPos = obj.startPos;
+    changeSlideAuto: function(){                        //метод объекта автоматически листающий слайды
+        setInterval(() => {                             //функция повторяющая перелистывание через (см. строка 52)    
+            for (item in this.units){                   //Проходим циклом по слайдам (считает индексы)
+                let obj = this.units[item];             //получаем объект по индексу
+                if (obj.curPos <= obj.endPos){          //если текущая позиция меньше или равна конечной
+                    obj.curPos = obj.startPos;          //устанавливаем стартовую
+                }
+                else{                                   //инача
+                    obj.curPos -= this.step;            //уменьшаем позицию на шаг (свайп вправо)
+                }
+    
+                this.units[item].item.style.left = `${obj.curPos}%`;    //устанавливаем left в нужное значение для сдвига слайда
             }
-            else{
-                obj.curPos -= this.step;
+        }, 8000);                                       //интервал повторения в мс
+    },
+
+    changeSideManual: function(eventer){            //метод перелистывания стрелками (аргумент - объект вызвавший метод стр79,80)
+        if (eventer === this.rightArrow){           //если объект - правая стрелка
+            for (item in this.units){               //проходим циклом по слайдам (считает индексы)
+                let obj = this.units[item];         //получаем объект слайда по индексу
+                if (obj.curPos <= obj.endPos){      //если текущая позиция меньше или равна конечной 
+                    break;                          //ничего не делаем
+                }
+                else{
+                    obj.curPos -= this.step;        //вычитаем из текущей позиции шаг         
+                }
+            this.units[item].item.style.left = `${obj.curPos}%`;    //пишем в left текушаю позицию
             }
-
-            // if (obj.curPos === 0){
-            //     obj.bullit.style.opacity = '1';
-            // }
-            // else{
-            //     obj.bullit.style.opacity = '0.5';
-            // }
-
-            this.units[item].item.style.left = `${obj.curPos}%`;
+        }
+        else if (eventer === this.leftArrow){       //по аналогии с правой только сдель левая стрелка
+            for (item in this.units){
+                let obj = this.units[item];
+                if (obj.curPos >= obj.startPos){    //поэтому текущаю позицию сравниваем со стартовой чтоб не листнуть лишнего    
+                    break;
+                }
+                else{
+                    obj.curPos += this.step;        //прибавсяем шаг (свайп влево)
+                }
+            this.units[item].item.style.left = `${obj.curPos}%`;    
+            }
         }
     }
 }
 
-setInterval(() => {
-    frames.changeBackgroundAuto()
-}, 5000);
+frames.leftArrow.addEventListener('click', () => frames.changeSideManual(frames.leftArrow));    //обработчики нажатия на стрелку
+frames.rightArrow.addEventListener('click', () => frames.changeSideManual(frames.rightArrow));
+
+frames.changeSlideAuto();                       //запускаем автосвайп
