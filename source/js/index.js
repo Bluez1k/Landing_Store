70f3beca162fc02f83881cb69dbdf77bb7ca5f3e
Blister-exist <teyvaz0kenaz@gmail.com>
2023-08-30 10:28:23 +0300

    added popup in index.js

--- source/js/index.js
+++ source/js/index.js
@@ -80,3 +80,29 @@ frames.leftArrow.addEventListener('click', () => frames.changeSideManual(frames.
 frames.rightArrow.addEventListener('click', () => frames.changeSideManual(frames.rightArrow));
 
 frames.changeSlideAuto();                       //запускаем автосвайп
+
+// Открыть popup
+document.getElementById("open-modal-btn").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open")
+})
+
+// Закрыть popup
+document.getElementById("close-my-modal-btn").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.remove("open")
+})
+
+// Закрыть popup при нажатии на Esc
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("my-modal").classList.remove("open")
+    }
+});
+
+// Закрыть popup при клике вне его
+document.querySelector("#my-modal .modal__box").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+document.getElementById("my-modal").addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
